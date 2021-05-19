0aeec9e3c227c523c5410cc1222d67867b041ee8
Carlos Gonzalez <carlos.gonzalez_arce@spaceforce.mil>
2021-05-18 22:44:21 -0600

    Initial commit

---
+++ JavascriptAsyncCheckpoint.js
@@ -0,0 +1,22 @@
+var fs = require('fs')
+const fetch = require('node-fetch')
+
+var input = 'input.txt'
+
+function getPkmTypeFromFile(input) {
+  fs.readFile(input, (err, data) => {
+    if (err) throw err;
+    const nameArray = data.toString().split('\r\n')
+    nameArray.map(res => getPkmType(res))
+  })
+
+}
+
+async function getPkmType(pkmName) {
+  await fetch (`https://pokeapi.co/api/v2/pokemon/${pkmName}`)
+    .then(res => res.json())
+    .then(res => res.types[0].type.name)
+    .then(res => console.log(`${pkmName}:`, res))
+}
+
+getPkmTypeFromFile(input)
